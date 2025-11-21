f1e256c96cebc47c9318226ae0ab762a5e1dcf70
Felipe <felipe@tupatrimonio.cl>
2025-11-21 17:50:48 -0300

    sidebar ok en tamaños y espacios

--- apps/web/src/components/ui/sidebar.tsx
+++ apps/web/src/components/ui/sidebar.tsx
@@ -13,9 +13,9 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/comp
 
 const SIDEBAR_COOKIE_NAME = 'sidebar:state'
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-const SIDEBAR_WIDTH = '16rem'
+const SIDEBAR_WIDTH = '12rem'
 const SIDEBAR_WIDTH_MOBILE = '18rem'
-const SIDEBAR_WIDTH_ICON = '3rem'
+const SIDEBAR_WIDTH_ICON = '4rem'
 const SIDEBAR_KEYBOARD_SHORTCUT = 'b'
 
 type SidebarContext = {
@@ -172,10 +172,13 @@ const Sidebar = React.forwardRef<
   return (
     <div
       ref={ref}
+      style={{
+        width: state === 'collapsed' ? SIDEBAR_WIDTH_ICON : SIDEBAR_WIDTH,
+        '--sidebar-width': SIDEBAR_WIDTH,
+        '--sidebar-width-icon': SIDEBAR_WIDTH_ICON,
+      } as React.CSSProperties}
       className={cn(
         'group peer hidden h-svh shrink-0 flex-col text-sidebar-foreground transition-[width] ease-linear md:flex',
-        'w-[--sidebar-width]',
-        'data-[state=collapsed]:w-[--sidebar-width-icon]',
         'data-[side=left]:border-r data-[side=right]:border-l',
         className,
       )}
